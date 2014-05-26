b43cd232180d1397e674d3f5f31f0564514bbc7a
wismer <matthewhl@gmail.com>
2014-05-25 22:02:39 -0400

    slight change to regex to look at beginning of path for match

--- http-server/app.rb
+++ http-server/app.rb
@@ -2,16 +2,16 @@ require './server.rb'
 require './route2.rb' 
 
 
-route1 = lambda do |captures|
+route1 = ->() {
   return "<html><head><title>Bleep Bloop</title></head><body>booooooork</body></html>"
-end
+}
 
-route2 = lambda do |captures|
+route2 = ->(captures) {
   return "hello " + captures["person"] + "!"
-end
+}
 
 router = Router.new
-router.add_route(/\/bleepbloop/, route1)
-router.add_route(/\/greet\/(?<person>[^\/]*)\/?/, route2)
+router.add_route(/^\/bleepbloop/, route1)
+router.add_route(/^\/greet\/(?<person>[^\/]*)\/?/, route2)
 
 Server.new({"port" => 9393, "router" => router}).start
